28623d2cb1514221107f30fe7e23dcce98a147e6
Harish Kannan <harishkannan2607@gmail.com>
2025-02-27 23:38:52 +0530

    Added Loader Animations 2

--- src/components/RobosoccerLoader.jsx
+++ src/components/RobosoccerLoader.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { Bot, Volleyball } from "lucide-react"; // Import bot and volleyball icon
-import "./RobosoccerLoader.css"; // Loader CSS
+import { Bot, Volleyball } from "lucide-react";
+import "./RobosoccerLoader.css";
 
 const RobosoccerLoader = () => {
   return (
     <div className="loader-container">
-      
       <div className="volleyball">
         <Volleyball size={80} className="text-white" />
       </div>
@@ -17,11 +16,8 @@ const RobosoccerLoader = () => {
       <div className="bot1">
         <Bot size={96} className="text-red-500" />
       </div>
-      
     </div>
   );
 };
-
-
 
 export default RobosoccerLoader;
